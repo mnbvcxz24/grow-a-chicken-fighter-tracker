43626403cb5a7d59f8d84198dedb0e56e37ece63
Albert <clivethology24@gmail.com>
2026-09-02 11:35:37 +0800

    Update 3

--- service-worker.js
+++ service-worker.js
@@ -37,3 +37,15 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window' }).then((clients) => {
+      for (const client of clients) {
+        if ('focus' in client) return client.focus();
+      }
+      if (self.clients.openWindow) return self.clients.openWindow('./index.html');
+    })
+  );
+});
